c1600b5104b1ff5658378d5bf9bf0a6c9727d799
Jarod Nidorino <jnidorin@pr-i2.kzn.21-school.ru>
2022-01-04 18:32:05 +0300

    completed makefile

---
+++ pipex.c
@@ -0,0 +1,23 @@
+#include "pipex.h"
+
+int	main(int argc, char **argv)
+{
+	int	input_fd;
+	int	output_fd;
+
+	if (argc == 5)
+	{
+
+	}
+	else
+		ft_putstr_fd();
+	return (1);
+}
+
+
+
+
+
+
+//cmd k, cmd c - комментарий
+//cmd k, cdm u - раскомментить
---
+++ pipex.h
@@ -0,0 +1,11 @@
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+
+void    ft_putstr_fd(char const *s, int fd);
+
+#endif
